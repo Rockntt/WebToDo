ca279b5922c0c7ce6e4dda27c4ba7c41b90a486d
Rockntt <klochkovmv2007@gmail.com>
2024-03-11 22:21:50 +0300

    little additions to auth pages

--- ToDo/forms.py
+++ ToDo/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import UserProfile
+from .models import *
 
 class UserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields
 
+
+class ToDoTaskForm(forms.ModelForm):
+    class Meta:
+        model = ToDoTask
+        fields = ['title', 'description']
