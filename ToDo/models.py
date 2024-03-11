82a96c04aafac8ce7c33449892e994b729c9a069
Rockntt <klochkovmv2007@gmail.com>
2024-03-11 21:08:42 +0300

    csrf fix

--- ToDo/models.py
+++ ToDo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class ToDoTask(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     done = models.BooleanField(default=False)
