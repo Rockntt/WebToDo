84f9c86f6e717cf012281678fcafb6c0b11ec055
Rockntt <klochkovmv2007@gmail.com>
2024-03-11 21:05:28 +0300

    front fix attempt

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import ToDoTask
 
 # Create your views here.
+
+def home(request):
+    tasks = ToDoTask.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        ToDoTask.objects.create(title=title, description=description)
+
+    return render(request, 'html/index.html', {'tasks': tasks})
